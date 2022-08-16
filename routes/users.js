bd479d5029e204c86cdaf1bb1c64f813d5fd6229
mohanraj-exe <smohan1029@gmail.com>
2022-08-16 17:54:43 +0530

    seventh commit

--- routes/users.js
+++ routes/users.js
@@ -44,10 +44,10 @@ router.get("/:id/verify/:token", async(req,res) =>{
 		const user = await User.findOne({_id: req.params.id});
 		if(!user) return res.status(400).send({message: "Invalid Link"})
 
-		const token = await Token.findOne({
-			userId: user._id,
-			token: req.params.token
-		})
+		const token = await Token.findOne(
+			{userId: user._id},
+			{token: req.params.token}
+		)
 		if(!token) return res.status(400).send({message: "Invalid Link"})
 
 		await User.updateOne({_id: user._id}, {verified: true})
